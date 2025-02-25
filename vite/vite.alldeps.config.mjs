d631d0a6453f416c3c884abd2a85403f60e6c4f0
Niolon <panikruth@gmail.com>
2025-02-25 11:10:12 +0000

    Add option to build a version with all batteries included

---
+++ vite/vite.alldeps.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import svgIconsPlugin from './vite-plugin-svg-icons';
+
+export default defineConfig({
+    plugins: [svgIconsPlugin()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, '../src/index.js'),
+            name: 'CifVis',
+            fileName: 'cifvis.alldeps',
+        },
+    },
+});
